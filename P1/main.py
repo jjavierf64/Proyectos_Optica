77833d9ca0e5d73db0ad1a5c25740b7ff0c7f5cd
jjavierf64 <jjavierf64@estudiantec.cr>
2022-08-04 16:25:12 -0600

    Indentation errors

--- P1/main.py
+++ P1/main.py
@@ -3,60 +3,61 @@ import matplotlib.pyplot as plt
 import math
 import numpy as np
 import re
-#primero se importan las librerias que se van a necesitar para realizar el codigo.
+# primero se importan las librerias que se van a necesitar para realizar el codigo.
 
 
 def run():
-    
-	#Ahora realizamos la lectura del csv usando pandas y limpiamos todos los datos de valores no numéricos con la función de pandas dropna.
-	data = pd.read_csv("P1/reflexion.csv")
-	data.dropna(inplace=True)
 
-	#Luego, asignamos los datos de cada columna a una variable.
-	anguloI=pd.DataFrame(data.anguloI)
-	anguloI.columns = ['ÁnguloI']
-	anguloRX=pd.DataFrame(data.anguloRX)
-	anguloRX.columns=["ÁnguloRX"]
-	plt.style.use('ggplot')
-	plt.scatter(anguloI,anguloRX,color= 'r',label='Dispersión de incidencia y reflexión.')
+    # Ahora realizamos la lectura del csv usando pandas y limpiamos todos los datos de valores no numéricos con la función de pandas dropna.
+    data = pd.read_csv("P1/reflexion.csv")
+    data.dropna(inplace=True)
 
-	# Adding axis labels
-	plt.xlabel('Ángulo de incidencia [°]')
-	plt.ylabel('Ángulo de reflexión [°]')
-	plt.title('Lecturas de ángulos.')
+    # Luego, asignamos los datos de cada columna a una variable.
+    anguloI = pd.DataFrame(data.anguloI)
+    anguloI.columns = ['ÁnguloI']
+    anguloRX = pd.DataFrame(data.anguloRX)
+    anguloRX.columns = ["ÁnguloRX"]
+    plt.style.use('ggplot')
+    plt.scatter(anguloI, anguloRX, color='r',
+                label='Dispersión de incidencia y reflexión.')
 
-	#Legend
-	plt.legend(loc=0)
-	plt.tight_layout()
-	plt.grid()
-	plt.show()
-	
+    # Adding axis labels
+    plt.xlabel('Ángulo de incidencia [°]')
+    plt.ylabel('Ángulo de reflexión [°]')
+    plt.title('Lecturas de ángulos.')
 
-    ### Refracción
-    #De nuevo se realiza la lectura del csv usando pandas y limpiamos todos los datos de valores no numéricos con la función de pandas dropna.
-	data = pd.read_csv("P1/refraccionA.csv")
-	data.dropna(inplace=True)
+    # Legend
+    plt.legend(loc=0)
+    plt.tight_layout()
+    plt.grid()
+    plt.show()
 
-    #Luego, asignamos los datos de cada columna a una variable.
-	aire = pd.DataFrame(data.aire)
-	aire.columns = ["Aire"]
 
-	agua = pd.DataFrame(data.agua)
-	agua.columns = ['Agua']
+# Refracción
+# De nuevo se realiza la lectura del csv usando pandas y limpiamos todos los datos de valores no numéricos con la función de pandas dropna.
+    data = pd.read_csv("P1/refraccionA.csv")
+    data.dropna(inplace=True)
 
-	plt.style.use('ggplot')
-	plt.scatter(aire,
+# Luego, asignamos los datos de cada columna a una variable.
+    aire = pd.DataFrame(data.aire)
+    aire.columns = ["Aire"]
+
+    agua = pd.DataFrame(data.agua)
+    agua.columns = ['Agua']
+
+    plt.style.use('ggplot')
+    plt.scatter(aire,
                 agua,
                 color='r',
                 label='Dispersión de incidencia y refracción.')
 
-    # Adding axis labels
-	plt.xlabel('Ángulo de incidencia [°]')
-	plt.ylabel('Ángulo de refracción [°]')
-	plt.title('Lecturas de ángulos.')
+# Adding axis labels
+    plt.xlabel('Ángulo de incidencia [°]')
+    plt.ylabel('Ángulo de refracción [°]')
+    plt.title('Lecturas de ángulos.')
 
-    #Legend
-	plt.legend(loc=0)
-	plt.tight_layout()
-	plt.grid()
-	plt.show()
+# Legend
+    plt.legend(loc=0)
+    plt.tight_layout()
+    plt.grid()
+    plt.show()
